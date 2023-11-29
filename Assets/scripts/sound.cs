b46daaa7a2a2acb5f6c4ab0606f41d1368080263
darrenclide <542593@escg.ac.uk>
2023-11-29 21:42:52 +0000

    add

---
+++ Assets/scripts/sound.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound : MonoBehaviour
+{
+    public AudioSource soundPlay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundPlay.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
